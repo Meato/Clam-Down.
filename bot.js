d4660466db83de2896ab29af1468781e09e60a87
Meato <41707872+Meato@users.noreply.github.com>
2018-08-03 20:21:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -726,5 +726,21 @@ ReBeL.guild.roles.filter(rebel => isNaN(rebel)).forEach(codes => codes.delete())
 
 
 
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`برا ${x}`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
