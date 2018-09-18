e7403caf135f575ae9d408ff9901748eba440404
Meato <41707872+Meato@users.noreply.github.com>
2018-09-18 03:45:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -612,11 +612,6 @@ client.on('message', message => {
     const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
         .setDescription(" ✅    تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
 **
 
 -${message.guild.name}  Link
