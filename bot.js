79751a5bb4ecb275d878432087f92b0be6983e02
Meato <41707872+Meato@users.noreply.github.com>
2018-09-29 19:48:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -743,6 +743,18 @@ if (message.content === '!spam') {
 
 
 
-
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("495637931936055316").send({ embed: stewart });
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
