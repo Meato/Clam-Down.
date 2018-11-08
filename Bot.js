84566304b6fd20bdbb10b37ed6b42f0e4d8078b9
Meato <41707872+Meato@users.noreply.github.com>
2018-11-08 14:24:49 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.on('ready', () => {
   console.log(Logged in as ${client.user.tag}!);
 });
-
 
 client.on('ready', async() => {
 var server = "Id server";510045258139303946
@@ -13,5 +11,7 @@ var channel = "id room";510045318860242944
     client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
     },305);
 })
+
+
 
 client.login(process.env.BOT_TOKEN)
